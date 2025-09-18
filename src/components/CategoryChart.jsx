826ec84d8a787a4dc2bd0f6a900435877c8406f3
anmolugale13 <anmolugale13@gmail.com>
2025-09-18 22:50:00 +0530

    Fix Chart.js bar chart error by registering LinearScale

--- src/components/CategoryChart.jsx
+++ src/components/CategoryChart.jsx
@@ -1,9 +1,9 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { Pie, Bar } from 'react-chartjs-2';
-import { Chart as ChartJS, ArcElement, Tooltip, Legend, CategoryScale, BarElement } from 'chart.js';
+import { Chart as ChartJS, ArcElement, Tooltip, Legend, CategoryScale, LinearScale, BarElement } from 'chart.js';
 
-ChartJS.register(ArcElement, Tooltip, Legend, CategoryScale, BarElement);
+ChartJS.register(ArcElement, Tooltip, Legend, CategoryScale, LinearScale, BarElement);
 
 export default function CategoryChart({ chartData, chartType }) {
   if (!chartData) return null;
